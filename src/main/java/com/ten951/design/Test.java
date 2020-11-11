3373829242e3c95a52653d9534c50ef98679b981
wangyongtian <wangyongtian@daojia-inc.com>
2020-11-11 14:38:49 +0800

    生产者-消费者

---
+++ src/main/java/com/ten951/design/Test.java
@@ -0,0 +1,136 @@
+package com.ten951.design;
+
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author 王永天
+ * @date 2020-11-03 14:01
+ */
+public class Test<T> {
+    private final BlockingQueue<T> queue = new ArrayBlockingQueue<>(400);
+    ExecutorService executorService = new ThreadPoolExecutor(1, 1, 60L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(30));
+    private ReentrantLock lock = new ReentrantLock();
+
+    public enum TaskType {
+        sub1, sub2, sub3;
+    }
+
+    public void cu() throws InterruptedException {
+        // CompletionService service = new ExecutorCompletionService<>(executorService);
+        Map<TaskType, Boolean> map = new ConcurrentHashMap<>(3);
+        map.put(TaskType.sub1, Boolean.FALSE);
+        map.put(TaskType.sub2, Boolean.FALSE);
+        map.put(TaskType.sub3, Boolean.FALSE);
+        CountDownLatch latch = new CountDownLatch(1);
+        Thread1 objectThread1 = new Thread1(latch, map);
+        Thread2 objectThread2 = new Thread2(latch, map);
+        Thread3 objectThread3 = new Thread3(latch, map);
+       /* Future<String> submit = executorService.submit(objectThread1);
+        Future<String> submit2 = executorService.submit(objectThread2);*/
+        Future<String> submit3 = executorService.submit(objectThread3);
+        Future<String> submit31 = executorService.submit(objectThread3);
+        Future<String> submit32 = executorService.submit(objectThread3);
+        System.out.println("执行关闭");
+        executorService.shutdownNow();
+        /*service.submit(() -> {
+            Thread.sleep(1000L);
+            throw new NullPointerException();
+           // return "T1";
+        });
+        service.submit(() -> {
+            Thread.sleep(2000L);
+            return "T2";
+        });
+        service.submit(() -> {
+            Thread.sleep(3000L);
+            return "T3";
+        });*/
+        // Object o = service.take().get();
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        new Test<>().cu();
+
+    }
+
+
+    private static class Thread1 implements Callable<String> {
+        private final CountDownLatch latch;
+        private final Map<TaskType, Boolean> map;
+
+        public Thread1(CountDownLatch latch, Map<TaskType, Boolean> map) {
+            this.latch = latch;
+            this.map = map;
+        }
+
+        @Override
+        public String call() {
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                //重新中断
+                Thread.currentThread().interrupt();
+            } finally {
+                latch.countDown();
+            }
+            map.put(TaskType.sub1, Boolean.TRUE);
+            return "T1执行完成";
+        }
+    }
+
+    private static class Thread2 implements Callable<String> {
+        private final CountDownLatch latch;
+        private final Map<TaskType, Boolean> map;
+
+        public Thread2(CountDownLatch latch, Map<TaskType, Boolean> map) {
+            this.latch = latch;
+            this.map = map;
+        }
+
+        @Override
+        public String call() {
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                //重新中断
+                Thread.currentThread().interrupt();
+            } finally {
+                latch.countDown();
+            }
+            map.put(TaskType.sub2, Boolean.TRUE);
+            return "T2执行完成";
+        }
+    }
+
+    private static class Thread3 implements Callable<String> {
+        private final CountDownLatch latch;
+        private final Map<TaskType, Boolean> map;
+
+        public Thread3(CountDownLatch latch, Map<TaskType, Boolean> map) {
+            this.latch = latch;
+            this.map = map;
+        }
+
+        @Override
+        public String call() {
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                //重新中断
+                Thread.currentThread().interrupt();
+            } finally {
+                latch.countDown();
+            }
+            if (!Thread.currentThread().isInterrupted())  {
+                System.out.println("继续执行");
+            }else {
+                System.out.println("中断了 不执行");
+            }
+            map.put(TaskType.sub3, Boolean.TRUE);
+            return "T3执行完成";
+        }
+    }
+}
--- src/main/java/com/ten951/design/twophase/TerminationToken.java
+++ src/main/java/com/ten951/design/twophase/TerminationToken.java
@@ -13,8 +13,12 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class TerminationToken {
 
+    // 停止标记位. 设置volatile 为了多线程情况下的可见性
     protected volatile boolean toShutdown = false;
+    // 记录次数
     public final AtomicInteger reservations = new AtomicInteger(0);
+    // WeakReference 当发生GC时就会被回收
+    // SoftReference 软引用. 当内存不足时. 会被GC回收
     private final Queue<WeakReference<Terminatable>> coordinatedThread;
 
     public TerminationToken() {
@@ -34,13 +38,13 @@ public class TerminationToken {
         coordinatedThread.add(new WeakReference<>(thread));
     }
 
-    protected void notifyThreadTermination(Terminatable thread) {
+    protected void notifyThreadTermination(Terminatable termin) {
         WeakReference<Terminatable> wrThread;
-        Terminatable otherThread;
+        Terminatable terminatable;
         while (null != (wrThread = coordinatedThread.poll())) {
-            otherThread = wrThread.get();
-            if (null != otherThread && otherThread != thread) {
-                otherThread.terminate();
+            terminatable = wrThread.get();
+            if (null != terminatable && terminatable != termin) {
+                terminatable.terminate();
             }
         }
 
